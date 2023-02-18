77741c758a9e796ad8390579a50047b71e7d1735
veoco <one@nomox.cn>
2023-02-18 21:04:52 +0800

    新增分类修改页面

---
+++ src/categories/pages/category_page.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Form, Button, Toast, Col, Row } from '@douyinfe/semi-ui';
+import { Link } from "wouter";
+import useSWR from 'swr'
+
+export default function CategoryPage({ params }) {
+  const { data, error, isLoading } = useSWR(`/api/categories/${params.slug}`);
+
+  useEffect(() => {
+    document.title = "分类修改";
+  })
+
+  if (error) return <div>加载失败</div>;
+  if (isLoading) return <div>正在加载</div>;
+
+  const handleSubmit = async (values) => {
+    const access_token = sessionStorage.getItem("access_token");
+    const r = await fetch(`/api/categories/${params.slug}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${access_token}`,
+      },
+      body: JSON.stringify(values),
+    })
+    if (r.status != 200) {
+      if (r.status == 400) {
+        Toast.error({ content: '参数错误' })
+      } else {
+        mapFail(r.status);
+      }
+      return;
+    }
+    setLocation("/admin/categories/");
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col span={8}>
+          <Form.Input field='name' label='名称' type="text" initValue={data.name} />
+        </Col>
+        <Col span={8} offset={1}>
+          <Form.Input field='slug' label="Slug" type="text" initValue={data.slug} />
+        </Col>
+      </Row>
+      <Row>
+        <Form.TextArea field='description' label="描述" initValue={data.description} />
+      </Row>
+      <div className="w-full mt-2">
+        <Button htmlType='submit' type="tertiary">修改</Button>
+      </div>
+    </Form>
+  )
+};
