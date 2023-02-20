233b195f775ea224d94714567c7724f50c8eaaa4
veoco <one@nomox.cn>
2023-02-20 13:01:15 +0800

    调整文章列表样式

--- src/posts/pages/posts_page.jsx
+++ src/posts/pages/posts_page.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
-import { List, Tag, ButtonGroup, Button, CheckboxGroup, Checkbox, Pagination } from '@douyinfe/semi-ui';
+import { Pagination } from '@douyinfe/semi-ui';
+import { Link } from "wouter";
 import useSWR from 'swr'
 
-export default function PostsPage(params) {
-  const [page, setPage] = useState(parseInt(params.page || '1'));
-  const { data, error, isLoading } = useSWR(`/api/posts/?page=${page}&private=true`);
-  const [checkedList, setCheckedList] = useState([]);
-  const [indeterminate, setIndeterminate] = useState(false);
-  const [checkAll, setCheckall] = useState(false);
+export default function PostsPage() {
+  const [page, setPage] = useState(1);
+  const { data, error, isLoading } = useSWR(`/api/posts/?page=${page}&page_size=15&private=true`);
 
   useEffect(() => {
     document.title = "文章列表";
@@ -16,62 +14,42 @@ export default function PostsPage(params) {
   if (error) return <div>加载失败</div>;
   if (isLoading) return <div>正在加载</div>;
 
-  const handleChange = (checkedList) => {
-    setCheckedList(checkedList);
-    setIndeterminate(!!checkedList.length && checkedList.length < data.count);
-    setCheckall(checkedList.length === data.count);
-  };
-
-  const handleCheckAllChange = (e) => {
-    let mids = [];
-    for (let category of data.results) {
-      mids.push(category.mid);
-    }
-    setCheckedList(e.target.checked ? mids : []);
-    setIndeterminate(false);
-    setCheckall(e.target.checked);
-  };
-
   const handlePageChange = (currentPage) => {
     setPage(currentPage);
   }
 
   return (
     <>
-      <div className="flex items-center bg-white border mb-3 p-4">
-        <Checkbox
-          className="px-2"
-          indeterminate={indeterminate}
-          onChange={handleCheckAllChange}
-          checked={checkAll}
-        >
-          <span className="ml-2">选择全部</span>
-        </Checkbox>
+      <div className="mb-1 flex items-center">
+        <h2 className="font-bold mr-auto">全部文章</h2>
+        <Link className="underline mr-1.5" href={`/posts/create`}>新增</Link>
       </div>
-      <CheckboxGroup value={checkedList} onChange={handleChange}>
-        <List className="border bg-white" dataSource={data.results} renderItem={item => {
+      <div>
+        {data.results.map(item => {
+          const created = new Date(item.created * 1000);
+          const modified = new Date(item.modified * 1000);
           return (
-            <List.Item
-              header={<Checkbox value={item.cid}></Checkbox>}
-              main={
-                <div className="flex">
-                  <Tag className="mr-2" color="blue">{item.status}</Tag>
-                  <span className="mr-1">{item.title}</span>
-                  <span className="text-sm text-slate-500">{item.slug}</span>
-                </div>
-              }
-              extra={
-                <ButtonGroup theme="borderless">
-                  <Button>编辑</Button>
-                  <Button type="danger">删除</Button>
-                </ButtonGroup>
-              }
-            />
+            <div className="my-1" key={item.cid}>
+              <h2><Link className="underline mr-1.5" href={`/posts/${item.slug}/`}>{item.title}</Link></h2>
+              <ul className="flex flex-wrap mt-1 text-gray-400 text-xs">
+                <li className="mr-2">创建于 {created.toLocaleDateString()}</li>
+                {item.created == item.modified ? "" : <li className="mr-2 last:mr-0" title={modified.toLocaleString()}>修改于 {modified.toLocaleDateString()}</li>}
+                {item.categories ? item.categories.map((cate) => {
+                  return (
+                    <li className="mr-2 underline last:mr-0"><Link href={`/categories/${cate.slug}/`} key={cate.mid}>{cate.name}</Link></li>
+                  )
+                }) : <li>未分类</li>}
+                {item.tags ? item.tags.map((tag) => {
+                  return (
+                    <li className="mr-2 underline before:content-['#'] last:mr-0"><Link href={`/tags/${tag.slug}/`} key={tag.mid}>{tag.name}</Link></li>
+                  )
+                }) : <li>无标签</li>}
+              </ul>
+            </div>
           )
-        }}
-        />
-      </CheckboxGroup>
-      <Pagination className="bg-white p-4 mt-3 border" total={data.all_count} pageSize={10} currentPage={page} onPageChange={handlePageChange} showQuickJumper></Pagination>
+        })}
+      </div>
+      <Pagination className={data.all_count > 15 ? "mt-2" : "hidden"} total={data.all_count} pageSize={15} currentPage={page} onPageChange={handlePageChange} showQuickJumper></Pagination>
     </>
   )
 };
