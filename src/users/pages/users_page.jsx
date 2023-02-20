82dab583e9ff6766b4f7180e3f81b3e6290dddfa
veoco <one@nomox.cn>
2023-02-20 13:16:07 +0800

    调整用户列表样式

--- src/users/pages/users_page.jsx
+++ src/users/pages/users_page.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
-import { List, Tag, ButtonGroup, Button, CheckboxGroup, Checkbox } from '@douyinfe/semi-ui';
+import { Pagination } from '@douyinfe/semi-ui';
+import { Link } from "wouter";
 import useSWR from 'swr'
 
 export default function UsersPage() {
-  const { data, error, isLoading } = useSWR(`/api/users/`);
-  const [checkedList, setCheckedList] = useState([]);
-  const [indeterminate, setIndeterminate] = useState(false);
-  const [checkAll, setCheckall] = useState(false);
+  const [page, setPage] = useState(1);
+  const { data, error, isLoading } = useSWR(`/api/users/?page=${page}&page_size=15`);
 
   useEffect(() => {
     document.title = "用户列表";
@@ -15,58 +14,31 @@ export default function UsersPage() {
   if (error) return <div>加载失败</div>;
   if (isLoading) return <div>正在加载</div>;
 
-  const handleChange = (checkedList) => {
-    setCheckedList(checkedList);
-    setIndeterminate(!!checkedList.length && checkedList.length < data.count);
-    setCheckall(checkedList.length === data.count);
-  };
-
-  const handleCheckAllChange = (e) => {
-    let uids = [];
-    for (let user of data.results) {
-      uids.push(user.uid);
-    }
-    setCheckedList(e.target.checked ? uids : []);
-    setIndeterminate(false);
-    setCheckall(e.target.checked);
-  };
+  const handlePageChange = (currentPage) => {
+    setPage(currentPage);
+  }
 
   return (
     <>
-      <div className="flex items-center bg-white border mb-3 p-4">
-        <Checkbox
-          className="px-2"
-          indeterminate={indeterminate}
-          onChange={handleCheckAllChange}
-          checked={checkAll}
-        >
-          <span className="ml-2">选择全部</span>
-        </Checkbox>
-        <span className="text-sm">共有 {data.all_count} 位用户</span>
+      <div className="mb-1 flex items-center">
+        <h2 className="font-bold mr-auto">全部用户</h2>
+        <Link className="underline mr-1.5" href={`/users/`}>新增</Link>
       </div>
-      <CheckboxGroup value={checkedList} onChange={handleChange}>
-        <List className="border bg-white" dataSource={data.results} renderItem={item => {
+      <div>
+        {data.results.map(item => {
+          const created = new Date(item.created * 1000);
+          const modified = new Date(item.modified * 1000);
           return (
-            <List.Item
-              header={<Checkbox value={item.uid}></Checkbox>}
-              main={
-                <div className="flex">
-                  <Tag className="mr-2" color="blue">{item.uid}</Tag>
-                  <span className="mr-1">{item.name}</span>
-                  <span className="text-sm text-slate-500">{item.screenName}</span>
-                </div>
-              }
-              extra={
-                <ButtonGroup theme="borderless">
-                  <Button>编辑</Button>
-                  <Button type="danger">删除</Button>
-                </ButtonGroup>
-              }
-            />
+            <div className="my-1" key={item.uid}>
+              <h2><Link className="underline mr-1.5" href={`/users/`}>{item.screenName}</Link></h2>
+              <ul className="flex flex-wrap mt-1 text-gray-400 text-xs">
+                <li className="mr-2">创建于 {created.toLocaleDateString()}</li>
+              </ul>
+            </div>
           )
-        }}
-        />
-      </CheckboxGroup>
+        })}
+      </div>
+      <Pagination className={data.all_count > 15 ? "mt-2" : "hidden"} total={data.all_count} pageSize={15} currentPage={page} onPageChange={handlePageChange} showQuickJumper></Pagination>
     </>
   )
 };
